a467cc4e994db9dcbe75933506782a444e6d3ec4
Robert Niederreiter <office@squarewave.at>
2013-01-10 16:55:38 +0100

    tests

--- src/node/ext/zcml/tests.py
+++ src/node/ext/zcml/tests.py
@@ -1,6 +1,5 @@
 # Copyright BlueDynamics Alliance - http://bluedynamics.com
 # GNU General Public License Version 2
-
 import os
 import unittest
 import doctest
@@ -10,20 +9,23 @@ from interlude import interact
 from zope.configuration.xmlconfig import XMLConfig
 import node.ext.xml
 
+
 optionflags = doctest.NORMALIZE_WHITESPACE | \
               doctest.ELLIPSIS | \
               doctest.REPORT_ONLY_FIRST_FAILURE
 
+
 TESTFILES = [
-    '../_api.rst',
+    '_api.rst',
 ]
 
-datadir = os.path.join(os.path.dirname(__file__), 'data')
+
+datadir = os.path.join(os.path.dirname(__file__), 'testing', 'data')
+
 
 def test_suite():
     XMLConfig('meta.zcml', zope.component)()
     XMLConfig('configure.zcml', node.ext.xml)()
-    
     return unittest.TestSuite([
         doctest.DocFileSuite(
             file, 
@@ -33,6 +35,7 @@ def test_suite():
                    'datadir': datadir,},
         ) for file in TESTFILES
     ])
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite') 
